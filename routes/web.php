383d9d57f30c3d6ca245b7b3703f5792fdaa1e21
dariuslingasa-png <darius.lingasa@gmail.com>
2026-06-19 05:50:59 +0800

    fix: change Google OAuth paths to bypass Mod_Security blocks

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 // Google OAuth
-Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])
+Route::get('/login/google', [GoogleAuthController::class, 'redirect'])
     ->middleware('throttle:10,1')
     ->name('auth.google');
-Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])
+Route::get('/login/google/callback', [GoogleAuthController::class, 'callback'])
     ->middleware('throttle:10,1')
     ->name('auth.google.callback');
 
